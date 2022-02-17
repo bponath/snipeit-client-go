16639f658b0153c3c483038250d7dae9d0eaa15f
Brian Ponath <bponath@gmail.com>
2022-02-17 16:16:29 -0600

    adding get single company

--- companies.go
+++ companies.go
@@ -27,6 +27,27 @@ func (c *Client) GetAllCompanies(authToken *string) (*[]CompanyRow, error) {
 	return &company.Rows, nil
 }
 
+// GetOrder - Returns a specifc order
+func (c *Client) GetCompany(companyID string, authToken *string) (*CompanyRow, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/companies/%s", c.HostURL, companyID), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.doRequest(req, authToken)
+	if err != nil {
+		return nil, err
+	}
+
+	company := CompanyRow{}
+	err = json.Unmarshal(body, &company)
+	if err != nil {
+		return nil, err
+	}
+
+	return &company, nil
+}
+
 // CreateCompany - Create new company
 func (c *Client) CreateCompany(company CompanyRow, authToken *string) (*CompanyRow, error) {
 	rb, err := json.Marshal(company)
